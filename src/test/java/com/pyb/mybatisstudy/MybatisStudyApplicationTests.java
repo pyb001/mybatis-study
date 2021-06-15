a0899422aa749d6f6d74bc0a5fcce109d32e0aa0
pyb5 <1835950530@qq.com>
2021-06-15 23:55:02 +0800

    第一次提交

---
+++ src/test/java/com/pyb/mybatisstudy/MybatisStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyb.mybatisstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
